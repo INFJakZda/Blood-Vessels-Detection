850626cbd56f67644c57bd8df19fb1c05f42c1c5
INFJakZda <zdanowski78@gmail.com>
2018-04-22 10:49:33 +0200

    Add morphology - test iterations (best 1) & morph mask

--- detector.py
+++ detector.py
@@ -5,12 +5,27 @@ import matplotlib.pyplot as plt
 
 def detect_vessel(org_image):
     copy_org_image = org_image.copy()
+    #make split of red green blue colors
     blue, green, red = cv2.split(org_image)
-    for clip in range(1, 40, 1):
-        clahe = cv2.createCLAHE(clipLimit=clip, tileGridSize=(8,8))
-        contrast_green = clahe.apply(green)
-        cv2.imwrite("out" + '/' + str(clip) + ".JPG", contrast_green)
+    #create a CLAHE object
+    clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(8,8))
+    contrast_green = clahe.apply(green)
     
+    r1 = cv2.morphologyEx(contrast_green, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations = 1)
+    R1 = cv2.morphologyEx(r1, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)), iterations = 1)
+
+    r2 = cv2.morphologyEx(R1, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11)), iterations = 1)
+    R2 = cv2.morphologyEx(r2, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(11,11)), iterations = 1)
+    
+    r3 = cv2.morphologyEx(R2, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(23,23)), iterations = 1)
+    R3 = cv2.morphologyEx(r3, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(23,23)), iterations = 1)	
+    f4 = cv2.subtract(R3,contrast_green)
+    f5 = clahe.apply(f4)
+    plt.imshow(f5)
+    plt.show()
+
+    return 0
+
 
 if __name__ == "__main__":
     data_catalog = "data"
